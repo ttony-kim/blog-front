bc4d3fd97c23a24fc1f8fab264561f3f2e71266f
Dasom Kim <ektha115@gmail.com>
2023-10-20 16:53:44 +0900

    feat: Post List 화면 추가
    1. Post List 화면 추가
    2. CORS 설정

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false, //true,
   swcMinify: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `http://localhost:8080/api/:path*`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/post/index.jsx
+++ pages/post/index.jsx
@@ -1,19 +1,57 @@
-import { List, ListItem, ListItemText } from "@mui/material";
+import {
+  Box,
+  Divider,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+} from "@mui/material";
+import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
+import { useEffect, useState } from "react";
+import Link from "next/link";
 
 export default function Post() {
-  console.log("hello");
+  const [data, setData] = useState({ count: 0, list: [] });
+
+  useEffect(() => {
+    const f = async () => {
+      await fetch("/api/posts")
+        .then((res) => {
+          if (!res.ok) {
+            throw new Error(`${res.status} 에러가 발생했습니다.`);
+          }
+          return res.json();
+        })
+        .then((json) => {
+          console.log(json);
+          setData({ count: json.count, list: json.list });
+        })
+        .catch((error) => console.log(error.message));
+    };
+    f();
+  }, []);
 
   return (
-    <List>
-      <ListItem>
-        <ListItemText primary="Single-line item" secondary="Secondary text" />
-      </ListItem>
-      <ListItem>
-        <ListItemText primary="Single-line item" secondary="Secondary text" />
-      </ListItem>
-      <ListItem>
-        <ListItemText primary="Single-line item" secondary="Secondary text" />
-      </ListItem>
-    </List>
+    <>
+      <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+        <span>Category ({data.count})</span>
+        <Link href="/post/edit">
+          <IconButton aria-label="delete">
+            <EditOutlinedIcon />
+          </IconButton>
+        </Link>
+      </Box>
+      <Divider />
+      <List>
+        {data.list.map((post) => (
+          <>
+            <ListItem key={post.id}>
+              <ListItemText primary={post.title} secondary={post.content} />
+            </ListItem>
+            <Divider />
+          </>
+        ))}
+      </List>
+    </>
   );
 }
