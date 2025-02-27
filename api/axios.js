5e318bb74c33ee2f905928761ff66f141c75d707
Dasom Kim <dskim@hanssak.co.kr>
2025-02-27 17:50:17 +0900

    [feat] axios request interceptor 추가

--- api/axios.js
+++ api/axios.js
@@ -5,6 +5,17 @@ const instance = axios.create({
   timeout: 1000,
 });
 
+// 요청 인터셉처 추가
+instance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+// 응답 인터셉터 추가
 instance.interceptors.response.use(
   (response) => {
     return response;
